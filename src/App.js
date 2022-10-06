d898fe4895e6d1be6fdb14bd4ad03dc5d328fdd5
Returnmakdo <cldud970@naver.com>
2022-10-06 11:26:38 +0900

    todolist 시험

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+let number = 3;
+function Todo({ todo }) {
+  return <div className="todo-title">{todo.title}</div>;
+}
+function List({ todos, setTodos }) {
+  return (
+    <div className="list-wrapper">
+      {todos.map((todo) => {
+        return <Todo todo={todo} key={todo.id} setTodos={setTodos} />;
+      })}
+    </div>
+  );
+}
+
+function Form({ setTodos, todos }) {
+  const init = {
+    id: 0,
+    title: "",
+  };
+  const [todo, setTodo] = useState(init);
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (todo.title.trim() === "") return alert("내용을 입력해주세요");
+    setTodos([...todos, { ...todo, id: number }]);
+    setTodo(init);
+    number++;
+  };
+  return (
+    <form onSubmit={onSubmitHandler} className="add-form">
+      <div className="input-group">
+        <input type="text" name="title" value={todo.title} className="add-input input-body" onChange={onChangeHandler} />
+      </div>
+      <button className="add-button">추가하기</button>
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "자바스크립트 배워보기",
+    },
+    {
+      id: 2,
+      title: "리액트 배워보기",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <Form setTodos={setTodos} todos={todos} />
+      <List setTodos={setTodos} todos={todos} />
     </div>
   );
 }
